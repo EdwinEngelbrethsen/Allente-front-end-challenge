5bad73e8ed18e61d217ac74d2755a2ec77b62b00
EdwinEngelbrethsen <edwin_engelbrethsen@outlook.com>
2021-06-17 14:50:14 +0200

    nested routing and folder structure

---
+++ allente-front-end-challenge/src/components/Topbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../style/component-style/Topbar.css"
+
+const Topbar = () => {
+
+    return (
+        <>
+        
+        </>
+    )
+}
---
+++ allente-front-end-challenge/src/pages/Homescreen.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Homescreen = () => {
+    return (
+        <div>
+            {/* Topbar */}
+            <h1>home</h1>
+            <p>this will be the homescreen</p>
+
+            {/* Banner */}
+
+            {/* Row */}
+
+            {/* Footer? */}
+        </div>
+    )
+}
+
+export default Homescreen;
